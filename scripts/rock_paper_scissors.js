213e01b9814e3b45a7e456cd60fca3c8a29e20cc
UnwaryBuffalo <jneaton922@gmail.com>
2021-05-28 22:20:45 -0400

    converted from console logging to buttons with DOM manipulation and eventListeners

--- scripts/rock_paper_scissors.js
+++ scripts/rock_paper_scissors.js
@@ -19,84 +19,126 @@ function cpuPick(){
     }
 }
 
-
-function playerPick() {
-    do{
-        input = prompt("Enter a selection of rock paper or scissors:","").toLowerCase();
-    } while (["rock","paper","scissors"].indexOf(input) < 0);
-    
-    return input;
-}
-
-
-
 function playRound(player, cpu){
-    console.log("The computer selected "+cpu);
+    alert_message = 'Your selection: '+player;
+    alert_message += "\nCPU selection: "+cpu+"\n\n";
     switch (cpu){
         case "rock":
             switch(player){
                 case ("rock"):
-                    console.log("It's a tie! Try again");
-                    return[0,0];
+                    alert_message+="It's a tie! Try again";
+                    // alert(alert_message);
+                    return[alert_message,0,0];
                 case ("paper"):
-                    console.log("paper covers rock! You win this round!");
-                    return [1,0];
+                    alert_message+="paper covers rock! You win this round!";
+                    // alert(alert_message);
+                    return [alert_message,1,0];
                 case ("scissors"):
-                    console.log("scissors get annihilated by rock! You lose this round...")
-                    return [0,1];
+                    alert_message+="scissors get annihilated by rock! You lose this round...";
+                    // alert(alert_message);
+                    return [alert_message,0,1];
             }
         case "paper":
             switch(player){
                 case ("rock"):
-                    console.log("paper covers rock! You lose this round...");
-                    return[0,1];
+                    alert_message+="paper covers rock! You lose this round...";
+                    // alert(alert_message);
+                    return[alert_message,0,1];
                 case ("paper"):
-                    console.log("It's a tie! Try again");
-                    return[0,0];
+                    alert_message+="It's a tie! Try again";
+                    // alert(alert_message);
+                    return[alert_message,0,0];
                 case ("scissors"):
-                    console.log("scissors cuts paper! You win this round!");
-                    return [1,0];
+                    alert_message+="scissors cuts paper! You win this round!";
+                    // alert(alert_message);
+                    return [alert_message,1,0];
             }
         case "scissors":
             switch(player){
                 case ("rock"):
-                    console.log("scissors get annihilated by rock! You win this round!");
-                    return [1,0];
+                    alert_message+="scissors get annihilated by rock! You win this round!"
+                    // alert(alert_message);
+                    return [alert_message,1,0];
                 case ("paper"):
-                    console.log("scissors cuts paper. You lose this round!");
-                    return [0,1];
+                    alert_message+="scissors cuts paper. You lose this round!";
+                    // alert(alert_message);
+                    return [alert_message,0,1];
                 case ("scissors"):
-                    console.log("It's a tie! Try again");
-                    return[0,0];
+                    alert_message+="It's a tie! Try again";
+                    // alert(alert_message);
+                    return[alert_message,0,0];
                     
             }
     }
 }
 
+const paperButton = document.querySelector('button#paper')
+const scissorsButton = document.querySelector('button#scissors')
+const rockButton = document.querySelector('button#rock')
+const resetButton = document.querySelector('button#reset');
 
-function game() {
-    console.log("Let's begin.");
-    console.log("Best of 5 wins!\n\n");
+resetButton.addEventListener('click', function(){
+    const text = document.querySelector('.container p');
+    text.textContent = ''
 
-    // player, cpu
-    let score = [0,0];
-    let thisRound = [0,0];
+    const playerscore = document.querySelector('#playerscore');
+    const cpuscore = document.querySelector('#cpuscore');
 
-    while (score[0] < 3 && score[1] < 3) {
-        thisRound = playRound(playerPick(), cpuPick());
-        score[0] += thisRound[0];
-        score[1] += thisRound[1];
-        console.log("the current scoreboard: You have " + score[0] +" and the cpu has "+ score[1]+ "\n\n\n");
+    playerscore.textContent = "Player: 0";
+    cpuscore.textContent = "CPU: 0";
+
+    document.querySelector('#rock').setAttribute('style','display: inline;')
+    document.querySelector('#paper').setAttribute('style','display: inline;')
+    document.querySelector('#scissors').setAttribute('style','display: inline;')
+    document.querySelector('#reset').setAttribute('style','display: none;')
+    
+
+})
+
+paperButton.addEventListener('click', function(){
+    updateDisplay(playRound('paper',cpuPick()));
+});
+
+rockButton.addEventListener('click', function (){
+    updateDisplay(playRound('rock',cpuPick()));
+});
+
+scissorsButton.addEventListener('click', function () {
+    updateDisplay(playRound('scissors',cpuPick()));
+});
+
+
+function updateDisplay(arr){
+    let message=arr[0];
+
+    const text = document.querySelector('.container p');
+    text.textContent = message
+
+    const playerscore = document.querySelector('#playerscore');
+    const cpuscore = document.querySelector('#cpuscore');
+
+    let player_currscore = parseInt(playerscore.textContent.split(' ')[1]);
+    console.log(player_currscore)
+    playerscore.value = player_currscore + arr[1];
+    playerscore.textContent = "Player: "+playerscore.value;
+
+    let cpu_currscore = parseInt(cpuscore.textContent.split(' ')[1]);
+    cpuscore.value = cpu_currscore + arr[2];
+    cpuscore.textContent = "CPU: "+cpuscore.value;
+
+    if (playerscore.value >=5){
+        text.textContent += "\n\nThe player has won 5 rounds! Game over!";
+        reset();
     }
- 
-    if (score[0] == 3){
-        console.log("\n\nYou win the game!");
+    if(cpuscore.value >=5){
+        text.textContent += "\n\nThe cpu has won 5 rounds! Game over!";
+        reset();
     }
-    else {
-        console.log("\n\nNice try! the cpu wins this game.")
-    }
-    return score;
 }
 
-
-
+function reset(){ 
+    document.querySelector('#rock').setAttribute('style','display: none;')
+    document.querySelector('#paper').setAttribute('style','display: none;')
+    document.querySelector('#scissors').setAttribute('style','display: none;')
+    document.querySelector('#reset').setAttribute('style','display: inline;')
+}
